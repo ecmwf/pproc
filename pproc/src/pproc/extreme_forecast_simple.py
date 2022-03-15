8ee9ea6081fae5f3da2c7b12ebfa7acbe689372e
Corentin Carton de Wiart <corentin.carton@ecmwf.int>
2022-03-15 15:57:36 +0000

    minor debug to extreme_forecast_simple and add eps factor

--- pproc/src/pproc/extreme_forecast_simple.py
+++ pproc/src/pproc/extreme_forecast_simple.py
@@ -9,7 +9,7 @@
 # granted to it by virtue of its status as an intergovernmental organisation nor
 # does it submit to any jurisdiction.
 
-
+import os
 import argparse
 import numpy as np
 
@@ -17,18 +17,18 @@ import eccodeshl
 from meteokit import extreme
 
 
-def compute_efi(fcs, clim):
+def compute_efi(fcs, clim, eps):
 
-    efi = extreme.efi(clim, fcs)
+    efi = extreme.efi(clim, fcs, eps)
 
     return efi
 
 
-def compute_sot(fcs, clim, sot_values):
+def compute_sot(fcs, clim, sot_values eps, missing):
 
     sot = {}
     for perc in sot_values:
-        sot[perc] = extreme.sot(clim, fcs, perc)
+        sot[perc] = extreme.sot(clim, fcs, perc, eps=eps, missing=missing)
 
     return sot
 
@@ -36,19 +36,24 @@ def compute_sot(fcs, clim, sot_values):
 def read_grib(in_file):
     reader = eccodeshl.FileReader(in_file)
     data = [message.get_array("values") for message in reader]
-    print(data)
+    #print(data)
 
-    return data
+    return np.asarray(data)
 
 
-def write_grib(template, data, out_dir, out_name):
+def write_grib(template, data, out_dir, out_name, missing=None):
     reader = eccodeshl.FileReader(template)
     messages = list(reader)
     message = messages[0]
+
+    if missing is not None:
+        message.set('missingValue', missing)
+
     message.set_array('values', data)
 
     out_file = os.path.join(out_dir, out_name)
-    message.write_to(out_file)
+    with open(out_file,"ab") as outfile:
+    	message.write_to(outfile)
 
 
 def main(args=None):
@@ -59,23 +64,31 @@ def main(args=None):
     parser.add_argument('template', help='GRIB template')
     parser.add_argument('out_dir', help='Output directory')
     parser.add_argument('--sot', nargs="+", type=int, help='SOT percentiles values')
-
+    parser.add_argument('--eps', type=float, help='epsilon factor')
+    
     args = parser.parse_args(args)
+    missing = -99999
 
-    sot_values = args.sot
-    if not isinstance(sot, list):
-        sot_values = [sot_values]
+    sot_num = args.sot
+    if not isinstance(sot_num, list):
+        sot_num = [sot_num]
     
     fcs = read_grib(args.fc_file)
     clim = read_grib(args.clim_file)
-
-    efi = compute_efi(fcs, clim)
-
-    sot = compute_sot(fcs, clim, sot_values)
-
-    write_grib(args.template, efi, args.out_dir, 'efi.grib')
-    for val in sot_values:
-        write_grib(args.template, sot[0], args.out_dir, f'sot_{val}.grib')
+    
+    fc_name = os.path.basename(args.fc_file).split('_')[0]
+    
+    efi = compute_efi(fcs, clim, eps=args.eps)
+    
+    efifile = 'efi' + os.path.basename(args.fc_file)[3:]
+    write_grib(args.template, efi, args.out_dir, efifile)
+    
+    if fc_name == 'eps':
+        sot = compute_sot(fcs, clim, sot_num eps=args.eps, missing=missing)
+        for val in sot_num:
+            sotfile = 'sot' + str(val) + os.path.basename(args.fc_file)[3:]
+            write_grib(args.template, sot[val], args.out_dir, sotfile, missing=missing)
+    
 
 if __name__ == "__main__":
     main()
