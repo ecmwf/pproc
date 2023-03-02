e6d661f96ba2dee22c5da046834a2f848dc5fde8
Jenny Wong <mawj@ad6-205.bullx>
2023-03-02 08:43:27 +0000

    Formatting

--- src/pproc/wind.py
+++ src/pproc/wind.py
@@ -20,13 +20,14 @@ import mir
 
 from pproc import common
 
+
 def retrieve_messages(cfg, req, cached_file):
     if cfg.vod2uv:
         print(req)
         common.fdb_read_to_file(cfg.fdb, req, cached_file)
         messages = mir_wind(cfg, cached_file)
     else:
-        out =  common.fdb_retrieve(cfg.fdb, req, cfg.interpolation_keys)
+        out = common.fdb_retrieve(cfg.fdb, req, cfg.interpolation_keys)
         reader = eccodes.StreamReader(out)
         messages = list(reader)
     return messages
@@ -38,40 +39,40 @@ def fdb_request_det(cfg, levelist, window):
     """
 
     req = cfg.request.copy()
-    req.pop('stream_ens')
-    req['stream'] = req.pop('stream_det')
+    req.pop("stream_ens")
+    req["stream"] = req.pop("stream_det")
     req["date"] = cfg.date.strftime("%Y%m%d")
-    req["time"] = cfg.date.strftime("%H")+'00'
-    if req['levtype'] != 'sfc':
-        req['levelist'] = levelist
-    req['step'] = window.steps
-    req['type'] = 'fc'
+    req["time"] = cfg.date.strftime("%H") + "00"
+    if req["levtype"] != "sfc":
+        req["levelist"] = levelist
+    req["step"] = window.steps
+    req["type"] = "fc"
 
     return retrieve_messages(cfg, req, f"wind_det_{levelist}_{window.name}.grb")
 
 
 def fdb_request_ens(cfg, levelist, window):
     """
-    Retrieve vorticity and divergence or u/v for ensemble forecast 
+    Retrieve vorticity and divergence or u/v for ensemble forecast
     (control + perturbed)
     """
 
     req = cfg.request.copy()
-    req.pop('stream_det')
-    req['stream'] = req.pop('stream_ens')
+    req.pop("stream_det")
+    req["stream"] = req.pop("stream_ens")
     req["date"] = cfg.date.strftime("%Y%m%d")
-    req["time"] = cfg.date.strftime("%H")+'00'
-    if req['levtype'] != 'sfc':
-        req['levelist'] = levelist
-    req['step'] = window.steps
+    req["time"] = cfg.date.strftime("%H") + "00"
+    if req["levtype"] != "sfc":
+        req["levelist"] = levelist
+    req["step"] = window.steps
 
     req_cf = req.copy()
-    req_cf['type'] = 'cf'
+    req_cf["type"] = "cf"
     messages = retrieve_messages(cfg, req_cf, f"wind_det_{levelist}_{window.name}.grb")
 
     req_pf = req.copy()
-    req_pf['type'] = 'pf'
-    req_pf['number'] = range(1, cfg.members+1)
+    req_pf["type"] = "pf"
+    req_pf["number"] = range(1, cfg.members + 1)
     messages += retrieve_messages(cfg, req_pf, f"wind_det_{levelist}_{window.name}.grb")
 
     return messages
@@ -107,10 +108,10 @@ def wind_speed(messages):
     wind_paramids = list(set([m["paramId"] for m in messages]))
     assert len(wind_paramids) == 2
 
-    u = {step:[] for step in steps}
-    v = {step:[] for step in steps}
+    u = {step: [] for step in steps}
+    v = {step: [] for step in steps}
     for m in messages:
-        step = m['step']
+        step = m["step"]
         param = m["paramId"]
         if param == wind_paramids[0]:
             u[step].append(m.get_array("values"))
@@ -124,29 +125,30 @@ def wind_speed(messages):
 
     ws = np.sqrt(u * u + v * v)
     ws = dict(zip(steps, ws))
-    
+
     return ws
+
 
 def basic_template(cfg, template, step, marstype):
     new_template = template.copy()
-    new_template.set('bitsPerValue', 24)
+    new_template.set("bitsPerValue", 24)
     new_template.set("marsType", marstype)
     new_template.set("step", step)
     if step == 0:
-        new_template.set('timeRangeIndicator', 1)
+        new_template.set("timeRangeIndicator", 1)
     else:
-        new_template.set('timeRangeIndicator', 0)
-    for key, value in cfg.options['grib_set'].items():
+        new_template.set("timeRangeIndicator", 0)
+    for key, value in cfg.options["grib_set"].items():
         new_template.set(key, value)
     return new_template
 
 
 def eps_speed_template(cfg, template, step, number):
     if number == 0:
-        eps_template = basic_template(cfg, template, step, 'cf')
+        eps_template = basic_template(cfg, template, step, "cf")
     else:
-        eps_template = basic_template(cfg, template, step, 'pf')
-        eps_template.set('number', number)
+        eps_template = basic_template(cfg, template, step, "pf")
+        eps_template.set("number", number)
     return eps_template
 
 
@@ -155,34 +157,44 @@ def write_output(cfg, filename, template, data):
     target_det = common.target_factory(cfg.target, out_file=file, fdb=cfg.fdb)
     common.write_grib(target_det, template, data)
 
+
 class ConfigExtreme(common.Config):
     def __init__(self, args):
         super().__init__(args)
 
-        self.date = datetime.strptime(str(self.options['fc_date']), "%Y%m%d%H")
-        self.root_dir = self.options['root_dir']
-        self.target = self.options['target']
+        self.date = datetime.strptime(str(self.options["fc_date"]), "%Y%m%d%H")
+        self.root_dir = self.options["root_dir"]
+        self.target = self.options["target"]
         self.out_dir = os.path.join(self.root_dir, self.date.strftime("%Y%m%d%H"))
 
         self.fdb = pyfdb.FDB()
 
-        self.request = self.options['request']
-        self.windows = self.options['windows']
-        self.levelist = self.options.get('levelist', [0])
-        self.interpolation_keys = self.options.get('interpolation_keys', None)
-        self.vod2uv = self.options.get('vod2uv', False)
+        self.request = self.options["request"]
+        self.windows = self.options["windows"]
+        self.levelist = self.options.get("levelist", [0])
+        self.interpolation_keys = self.options.get("interpolation_keys", None)
+        self.vod2uv = self.options.get("vod2uv", False)
 
         if args.eps_ws or args.eps_mean_std:
-            self.members = self.options['members']
+            self.members = self.options["members"]
 
 
 def main(args=None):
 
-    parser = common.default_parser('Calculate wind speed')
-    parser.add_argument('--det_ws', action='store_true', default=False, help='Wind speed for type=fc')
-    parser.add_argument('--eps_ws', action='store_true', default=False, help='Wind speed for type=pf/cf')
-    parser.add_argument('--eps_mean_std', action='store_true', default=False, help='Wind speed mean/std for type=pf/cf. ' +
-    'Default option if no options are set.')
+    parser = common.default_parser("Calculate wind speed")
+    parser.add_argument(
+        "--det_ws", action="store_true", default=False, help="Wind speed for type=fc"
+    )
+    parser.add_argument(
+        "--eps_ws", action="store_true", default=False, help="Wind speed for type=pf/cf"
+    )
+    parser.add_argument(
+        "--eps_mean_std",
+        action="store_true",
+        default=False,
+        help="Wind speed mean/std for type=pf/cf. "
+        + "Default option if no options are set.",
+    )
     args = parser.parse_args(args)
 
     # If no arguments are selected then run eps_mean_std by default
@@ -201,31 +213,48 @@ def main(args=None):
                 messages = fdb_request_det(cfg, levelist, window)
                 det = wind_speed(messages)
                 for step in window.steps:
-                    template_det = basic_template(cfg, messages[0], step, 'fc')
-                    write_output(cfg, f'det_{levelist}_{window.name}_{step}.grib', 
-                        template_det, det[step][0])
+                    template_det = basic_template(cfg, messages[0], step, "fc")
+                    write_output(
+                        cfg,
+                        f"det_{levelist}_{window.name}_{step}.grib",
+                        template_det,
+                        det[step][0],
+                    )
 
             # calculate wind speed, mean/stddev of wind speed for type=pf/cf (eps)
             if args.eps_ws or args.eps_mean_std:
                 messages = fdb_request_ens(cfg, levelist, window)
-                eps = wind_speed(messages) 
+                eps = wind_speed(messages)
                 template = messages[0]
                 for step in window.steps:
                     print(step)
                     if args.eps_ws:
                         for number in range(cfg.members + 1):
-                            template_eps = eps_speed_template(cfg, template, step, number)
-                            write_output(cfg, f'eps_{levelist}_{window.name}_{step}_{number}.grib', 
-                                template_eps, eps[step][number])
+                            template_eps = eps_speed_template(
+                                cfg, template, step, number
+                            )
+                            write_output(
+                                cfg,
+                                f"eps_{levelist}_{window.name}_{step}_{number}.grib",
+                                template_eps,
+                                eps[step][number],
+                            )
                     if args.eps_mean_std:
-                        template_mean = basic_template(cfg, template, step, 'em')
-                        write_output(cfg, f'mean_{levelist}_{window.name}_{step}.grib', 
-                            template_mean, np.mean(eps[step], axis=0))
+                        template_mean = basic_template(cfg, template, step, "em")
+                        write_output(
+                            cfg,
+                            f"mean_{levelist}_{window.name}_{step}.grib",
+                            template_mean,
+                            np.mean(eps[step], axis=0),
+                        )
 
-
-                        template_std = basic_template(cfg, template, step, 'es')
-                        write_output(cfg, f'std_{levelist}_{window.name}_{step}.grib', 
-                            template_std, np.std(eps[step], axis=0))
+                        template_std = basic_template(cfg, template, step, "es")
+                        write_output(
+                            cfg,
+                            f"std_{levelist}_{window.name}_{step}.grib",
+                            template_std,
+                            np.std(eps[step], axis=0),
+                        )
 
 
 if __name__ == "__main__":
