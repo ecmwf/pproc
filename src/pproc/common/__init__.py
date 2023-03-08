11b9c416a5434d772bdbf0cbe4c13e6c8de11391
Olivier Iffrig <olivier.iffrig@ecmwf.int>
2023-03-08 16:39:27 +0000

    Re-export ResourceMeter from pproc.common

--- src/pproc/common/__init__.py
+++ src/pproc/common/__init__.py
@@ -1,5 +1,6 @@
 from .io import fdb_read_to_file, fdb_read, write_grib, target_factory, fdb_retrieve, FDBTarget, fdb_read_with_template
 from .config import default_parser, Config
+from .resources import ResourceMeter
 from .window import (
     Window,
     SimpleOpWindow,
