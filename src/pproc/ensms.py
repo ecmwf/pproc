1e9fc1ec1a2069eaecc2699c4ff2e44d50b8c449
Olivier Iffrig <olivier.iffrig@ecmwf.int>
2023-03-09 12:03:59 +0000

    ensms: read and process data step by step

--- src/pproc/ensms.py
+++ src/pproc/ensms.py
@@ -49,17 +49,15 @@ def fdb_request_forecast(cfg, options, steps):
     return ens
 
 
-def ensemble_mean_std_eps(cfg, options, window):
+def ensemble_mean_std_eps(cfg, options, steps):
     """
     Calculate ensemble (type=cf/pf) mean and standard deviation of wind speed
     """
-    with common.ResourceMeter(f"Window {window.name}: read forecast"):
-        ens = fdb_request_forecast(cfg, options, window.steps)
-        template = ens.attrs['grib_template']
+    ens = fdb_request_forecast(cfg, options, steps)
+    template = ens.attrs['grib_template']
 
-    with common.ResourceMeter(f"Window {window.name}: compute mean/stddev"):
-        mean = ens.mean(dim='number')
-        stddev = ens.std(dim='number')
+    mean = ens.mean(dim='number')
+    stddev = ens.std(dim='number')
     print(mean)
     print(stddev)
 
@@ -98,8 +96,8 @@ class PressureLevels:
     def set_level_key(self, template, level):
         template.set('level', level)
 
-    def slice_dataset(self, ds, level, step):
-        return ds.sel(levelist=level, step=step).values
+    def slice_dataset(self, ds, level, **kwargs):
+        return ds.sel(levelist=level, **kwargs).values
 
 
 class SurfaceLevel:
@@ -110,8 +108,8 @@ class SurfaceLevel:
     def set_level_key(self, template, level):
         return
 
-    def slice_dataset(self, ds, level, step):
-        return ds.sel(step=step).values
+    def slice_dataset(self, ds, level, **kwargs):
+        return ds.sel(**kwargs).values
 
 
 def parameters_manager(options):
@@ -151,18 +149,19 @@ def main(args=None):
             window = common.Window(window_options)
 
             # calculate mean/stddev of wind speed for type=pf/cf (eps)
-            mean, std, template_ens = ensemble_mean_std_eps(cfg, options, window)
+            for step in window.steps:
+                with common.ResourceMeter(f"Window {window.name}, step {step}: compute mean/stddev"):
+                    mean, std, template_ens = ensemble_mean_std_eps(cfg, options, step)
 
-            with common.ResourceMeter(f"Window {window.name}: write output"):
-                for step in window.steps:
+                with common.ResourceMeter(f"Window {window.name}, step {step}: write output"):
                     for level in param_type.levels:
-                        mean_slice = param_type.slice_dataset(mean, level, step)
+                        mean_slice = param_type.slice_dataset(mean, level)
                         mean_file = os.path.join(cfg.out_dir, window.name, f'mean_{param}_{level}_{step}.grib')
                         target_mean = common.target_factory(cfg.target, out_file=mean_file, fdb=cfg.fdb)
                         template_mean = template_ensemble(cfg, param_type, template_ens, step, window.step, level, 'em')
                         common.write_grib(target_mean, template_mean, mean_slice)
 
-                        std_slice = param_type.slice_dataset(std, level, step)
+                        std_slice = param_type.slice_dataset(std, level)
                         std_file = os.path.join(cfg.out_dir, window.name, f'std_{param}_{level}_{step}.grib')
                         target_std = common.target_factory(cfg.target, out_file=std_file, fdb=cfg.fdb)
                         template_std = template_ensemble(cfg, param_type, template_ens, step, window.step, level, 'es')
