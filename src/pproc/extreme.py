11f63473c8c2a3f0f7ece5b542e8467dc2299e26
Jenny Wong <mawj@ac6-102.bullx>
2023-04-04 13:54:38 +0000

    Formatting

--- src/pproc/extreme.py
+++ src/pproc/extreme.py
@@ -28,32 +28,33 @@ def climatology_date(fc_date):
 
     # friday to monday -> take previous monday clim, else previous thursday clim
     if weekday == 0 or weekday > 3:
-        clim_date = fc_date - timedelta(days=(weekday+4)%7)
+        clim_date = fc_date - timedelta(days=(weekday + 4) % 7)
     else:
         clim_date = fc_date - timedelta(days=weekday)
 
     return clim_date
 
+
 class ExtremeVariables:
     def __init__(self, efi_cfg):
-        self.eps = float(efi_cfg['eps'])
-        self.sot = map(int, efi_cfg['sot'])
+        self.eps = float(efi_cfg["eps"])
+        self.sot = map(int, efi_cfg["sot"])
 
 
 def read_clim(cfg, clim_keys, window, n_clim=101):
 
     req = clim_keys.copy()
     req["date"] = cfg.clim_date.strftime("%Y%m%d")
-    req["time"] = '0000'
-    req["quantile"] = ['{}:100'.format(i) for i in range(n_clim)]
-    req["step"] = f'{window.name}'
+    req["time"] = "0000"
+    req["quantile"] = ["{}:100".format(i) for i in range(n_clim)]
+    req["step"] = f"{window.name}"
 
     print("Climatology request: ", req)
     da_clim = common.fdb_read(cfg.fdb, req)
-    da_clim_sorted = da_clim.reindex(quantile=[f'{x}:100' for x in range(n_clim)])
+    da_clim_sorted = da_clim.reindex(quantile=[f"{x}:100" for x in range(n_clim)])
     print(da_clim_sorted)
 
-    return np.asarray(da_clim_sorted.values), da_clim.attrs['grib_template']
+    return np.asarray(da_clim_sorted.values), da_clim.attrs["grib_template"]
 
 
 def extreme_template(window, template_fc, template_clim):
@@ -62,63 +63,67 @@ def extreme_template(window, template_fc, template_clim):
 
     for key, value in window.config_grib_header.items():
         template_ext[key] = value
-    
+
     # EFI specific stuff
-    template_ext['stepRange'] = window.name
-    if int(template_ext['timeRangeIndicator']) == 3:
-        template_ext['numberIncludedInAverage'] = len(window.steps)
-        template_ext['numberMissingFromAveragesOrAccumulations'] = 0        
+    template_ext["stepRange"] = window.name
+    if int(template_ext["timeRangeIndicator"]) == 3:
+        template_ext["numberIncludedInAverage"] = len(window.steps)
+        template_ext["numberMissingFromAveragesOrAccumulations"] = 0
 
     # set clim keys
     clim_keys = [
-        'powerOfTenUsedToScaleClimateWeight',
-        'weightAppliedToClimateMonth1',
-        'firstMonthUsedToBuildClimateMonth1',
-        'lastMonthUsedToBuildClimateMonth1',
-        'firstMonthUsedToBuildClimateMonth2',
-        'lastMonthUsedToBuildClimateMonth2',
-        'numberOfBitsContainingEachPackedValue'
+        "powerOfTenUsedToScaleClimateWeight",
+        "weightAppliedToClimateMonth1",
+        "firstMonthUsedToBuildClimateMonth1",
+        "lastMonthUsedToBuildClimateMonth1",
+        "firstMonthUsedToBuildClimateMonth2",
+        "lastMonthUsedToBuildClimateMonth2",
+        "numberOfBitsContainingEachPackedValue",
     ]
     for key in clim_keys:
         template_ext[key] = template_clim[key]
 
     # set fc keys
     fc_keys = [
-        'date',
-        'subCentre',
-        'totalNumber',
+        "date",
+        "subCentre",
+        "totalNumber",
     ]
     for key in fc_keys:
         template_ext[key] = template_fc[key]
-    
+
     return template_ext
 
 
 def efi_template(template):
     template_efi = template.copy()
-    template_efi['marsType'] = 27
-    template_efi['efiOrder'] = 0
-    template_efi['number'] = 0
+    template_efi["marsType"] = 27
+    template_efi["efiOrder"] = 0
+    template_efi["number"] = 0
     return template_efi
+
 
 def efi_template_control(template):
     template_efi = template.copy()
-    template_efi['marsType'] = 28
-    template_efi['efiOrder'] = 0
-    template_efi['totalNumber'] = 1
-    template_efi['number'] = 0
+    template_efi["marsType"] = 28
+    template_efi["efiOrder"] = 0
+    template_efi["totalNumber"] = 1
+    template_efi["number"] = 0
     return template_efi
+
 
 def sot_template(template, sot):
     template_sot = template.copy()
-    template_sot['marsType'] = 38
-    template_sot['number'] = sot
+    template_sot["marsType"] = 38
+    template_sot["number"] = sot
     if sot == 90:
-        template_sot['efiOrder'] = 99
+        template_sot["efiOrder"] = 99
     elif sot == 10:
-        template_sot['efiOrder'] = 1
+        template_sot["efiOrder"] = 1
     else:
-        raise Exception("SOT value '{sot}' not supported in template! Only accepting 10 and 90")
+        raise Exception(
+            "SOT value '{sot}' not supported in template! Only accepting 10 and 90"
+        )
     return template_sot
 
 
@@ -126,28 +131,32 @@ class ConfigExtreme(common.Config):
     def __init__(self, args):
         super().__init__(args)
 
-        self.fc_date = datetime.strptime(str(self.options['fc_date']), "%Y%m%d%H")
+        self.fc_date = datetime.strptime(str(self.options["fc_date"]), "%Y%m%d%H")
 
-        self.members = int(self.options['members'])
+        self.members = int(self.options["members"])
         self.fdb = pyfdb.FDB()
-        
-        self.root_dir = self.options['root_dir']
-        self.out_dir = os.path.join(self.root_dir, 'efi_test', self.fc_date.strftime("%Y%m%d%H"))
 
-        self.clim_date = self.options.get('clim_date', climatology_date(self.fc_date))
+        self.root_dir = self.options["root_dir"]
+        self.out_dir = os.path.join(
+            self.root_dir, "efi_test", self.fc_date.strftime("%Y%m%d%H")
+        )
 
-        self.target = self.options['target']
-        self.output_grib_set = self.options['grib_set']
+        self.clim_date = self.options.get("clim_date", climatology_date(self.fc_date))
 
-        print(f'Forecast date is {self.fc_date}')
-        print(f'Climatology date is {self.clim_date}')
-        print(f'Root directory is {self.root_dir}')
+        self.target = self.options["target"]
+        self.output_grib_set = self.options["grib_set"]
+
+        print(f"Forecast date is {self.fc_date}")
+        print(f"Climatology date is {self.clim_date}")
+        print(f"Root directory is {self.root_dir}")
 
 
 def main(args=None):
     sys.stdout.reconfigure(line_buffering=True)
 
-    parser = common.default_parser('Compute EFI and SOT from forecast and climatology for one parameter')
+    parser = common.default_parser(
+        "Compute EFI and SOT from forecast and climatology for one parameter"
+    )
     args = parser.parse_args(args)
     cfg = ConfigExtreme(args)
     recovery = common.Recovery(cfg.root_dir, args.config, cfg.fc_date, args.recover)
@@ -179,42 +188,61 @@ def main(args=None):
                 completed_windows = window_manager.update_windows(step, data)
                 for window in completed_windows:
 
-                    clim, template_clim = read_clim(cfg, param_cfg['clim_keys'], window)
-                    print(f'Climatology array: {clim.shape}')
+                    clim, template_clim = read_clim(cfg, param_cfg["clim_keys"], window)
+                    print(f"Climatology array: {clim.shape}")
 
-                    template_extreme = extreme_template(window, message_template, template_clim)
+                    template_extreme = extreme_template(
+                        window, message_template, template_clim
+                    )
 
-                    print('Computing efi for the control member')
-                    efi_control = extreme.efi(clim, window.step_values[: 1], efi_vars.eps)
+                    print("Computing efi for the control member")
+                    efi_control = extreme.efi(
+                        clim, window.step_values[:1], efi_vars.eps
+                    )
                     template_efi = efi_template_control(template_extreme)
-                    
-                    out_file = os.path.join(cfg.out_dir, f'efi_control_{param_name}_{window.suffix}.grib')
-                    target = common.target_factory(cfg.target, out_file=out_file, fdb=cfg.fdb)
+
+                    out_file = os.path.join(
+                        cfg.out_dir, f"efi_control_{param_name}_{window.suffix}.grib"
+                    )
+                    target = common.target_factory(
+                        cfg.target, out_file=out_file, fdb=cfg.fdb
+                    )
                     common.write_grib(target, template_efi, efi_control)
 
-                    print('Computing efi')
+                    print("Computing efi")
                     efi = extreme.efi(clim, window.step_values, efi_vars.eps)
                     template_efi = efi_template(template_extreme)
-                    
-                    out_file = os.path.join(cfg.out_dir, f'efi_{param_name}_{window.suffix}.grib')
-                    target = common.target_factory(cfg.target, out_file=out_file, fdb=cfg.fdb)
+
+                    out_file = os.path.join(
+                        cfg.out_dir, f"efi_{param_name}_{window.suffix}.grib"
+                    )
+                    target = common.target_factory(
+                        cfg.target, out_file=out_file, fdb=cfg.fdb
+                    )
                     common.write_grib(target, template_efi, efi)
 
                     sot = {}
                     for perc in efi_vars.sot:
-                        print(f'Computing sot {perc}')
-                        
-                        sot[perc] = extreme.sot(clim, window.step_values, perc, efi_vars.eps)
+                        print(f"Computing sot {perc}")
+
+                        sot[perc] = extreme.sot(
+                            clim, window.step_values, perc, efi_vars.eps
+                        )
                         template_sot = sot_template(template_extreme, perc)
-                        
-                        out_file = os.path.join(cfg.out_dir, f'sot{perc}_{param_name}_{window.suffix}.grib')
-                        target = common.target_factory(cfg.target, out_file=out_file, fdb=cfg.fdb)
+
+                        out_file = os.path.join(
+                            cfg.out_dir, f"sot{perc}_{param_name}_{window.suffix}.grib"
+                        )
+                        target = common.target_factory(
+                            cfg.target, out_file=out_file, fdb=cfg.fdb
+                        )
                         common.write_grib(target, template_sot, sot[perc])
 
             cfg.fdb.flush()
             recovery.add_checkpoint(param_name, step)
 
     recovery.clean_file()
+
 
 if __name__ == "__main__":
     main()
