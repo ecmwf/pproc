b06d080a4998bb7eb0673378cc116fd68f26defc
Olivier Iffrig <olivier.iffrig@ecmwf.int>
2023-05-03 09:50:07 +0000

    Fix Step class initialisation

--- src/pproc/common/steps.py
+++ src/pproc/common/steps.py
@@ -9,12 +9,13 @@ class Step:
     end: int = None
 
     def __init__(self, start_or_step, end=None):
+        _set_frozen = lambda attr, val: object.__setattr__(self, attr, val)
         if isinstance(start_or_step, Step):
-            self.start = start_or_step.start
-            self.end = start_or_step.end
+            _set_frozen("start", start_or_step.start)
+            _set_frozen("end", start_or_step.end)
         else:
-            self.start = int(start_or_step)
-            self.end = None if end is None else int(end)
+            _set_frozen("start", int(start_or_step))
+            _set_frozen("end", None if end is None else int(end))
 
     def __str__(self):
         if self.end is None:
