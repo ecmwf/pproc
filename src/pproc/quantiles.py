f2ac318461fd474dd53d628c6b21fe789c49777b
Olivier Iffrig <olivier.iffrig@ecmwf.int>
2023-06-06 15:54:32 +0000

    Reformat quantiles.py

--- src/pproc/quantiles.py
+++ src/pproc/quantiles.py
@@ -1,4 +1,3 @@
-
 import argparse
 import functools
 import sys
@@ -11,15 +10,30 @@ from meteokit.stats import iter_quantiles
 
 from pproc.common.config import Config, default_parser
 from pproc.common.dataset import open_multi_dataset
-from pproc.common.io import FileSetTarget, FileTarget, Target, missing_to_nan, nan_to_missing, read_template, target_from_location
-from pproc.common.parallel import QueueingExecutor, SynchronousExecutor, parallel_data_retrieval, shared_list
+from pproc.common.io import (
+    FileSetTarget,
+    FileTarget,
+    Target,
+    missing_to_nan,
+    nan_to_missing,
+    read_template,
+    target_from_location,
+)
+from pproc.common.parallel import (
+    QueueingExecutor,
+    SynchronousExecutor,
+    parallel_data_retrieval,
+    shared_list,
+)
 from pproc.common.resources import ResourceMeter
 from pproc.common.steps import AnyStep
 from pproc.common.window import Window
 from pproc.common.window_manager import WindowManager
 
 
-def read_ensemble(sources: dict, loc: str, members: int, dtype=np.float32, **kwargs) -> Tuple[eccodes.GRIBMessage, np.ndarray]:
+def read_ensemble(
+    sources: dict, loc: str, members: int, dtype=np.float32, **kwargs
+) -> Tuple[eccodes.GRIBMessage, np.ndarray]:
     """Read data from a GRIB file as a single array
 
     Parameters
@@ -42,9 +56,11 @@ def read_ensemble(sources: dict, loc: str, members: int, dtype=np.float32, **kwa
     numpy array (nfields, npoints)
         Read data
     """
+
     def set_number(keys):
-        if keys.get('type') == 'pf':
-            keys['number'] = range(1, members)
+        if keys.get("type") == "pf":
+            keys["number"] = range(1, members)
+
     readers = open_multi_dataset(sources, loc, update=set_number, **kwargs)
     template = None
     data = None
@@ -56,9 +72,11 @@ def read_ensemble(sources: dict, loc: str, members: int, dtype=np.float32, **kwa
                 raise EOFError(f"No data in {loc!r}")
             if template is None:
                 template = message
-                data = np.empty((members, template.get('numberOfDataPoints')), dtype=dtype)
+                data = np.empty(
+                    (members, template.get("numberOfDataPoints")), dtype=dtype
+                )
             for message in reader:
-                i = message.get('perturbationNumber', 0)
+                i = message.get("perturbationNumber", 0)
                 data[i, :] = missing_to_nan(message)
                 n_read += 1
     if n_read != members:
@@ -66,7 +84,14 @@ def read_ensemble(sources: dict, loc: str, members: int, dtype=np.float32, **kwa
     return template, data
 
 
-def do_quantiles(ens: np.ndarray, template: eccodes.GRIBMessage, target: Target, out_paramid: Optional[str] = None, n: int = 100, out_keys: Optional[Dict[str, Any]] = None):
+def do_quantiles(
+    ens: np.ndarray,
+    template: eccodes.GRIBMessage,
+    target: Target,
+    out_paramid: Optional[str] = None,
+    n: int = 100,
+    out_keys: Optional[Dict[str, Any]] = None,
+):
     """Compute quantiles
 
     Parameters
@@ -84,7 +109,7 @@ def do_quantiles(ens: np.ndarray, template: eccodes.GRIBMessage, target: Target,
     out_keys: dict, optional
         Extra GRIB keys to set on the output
     """
-    for i, quantile in enumerate(iter_quantiles(ens, n, method='sort')):
+    for i, quantile in enumerate(iter_quantiles(ens, n, method="sort")):
         message = template.copy()
         if out_keys is not None:
             message.set(out_keys)
@@ -108,11 +133,11 @@ def get_parser() -> argparse.ArgumentParser:
 class ParamConfig:
     def __init__(self, name, options: Dict[str, Any]):
         self.name = name
-        self.in_paramid = options['in']
-        self.out_paramid = options.get('out', None)
-        self._in_keys = options.get('in_keys', {})
-        self._out_keys = options.get('out_keys', {})
-        self._windows = options.get('windows', None)
+        self.in_paramid = options["in"]
+        self.out_paramid = options.get("out", None)
+        self._in_keys = options.get("in_keys", {})
+        self._out_keys = options.get("out_keys", {})
+        self._windows = options.get("windows", None)
 
     def in_keys(self, base: Optional[Dict[str, Any]] = None, **kwargs):
         keys = base.copy() if base is not None else {}
@@ -135,10 +160,12 @@ class ParamConfig:
         for coarse_cfg in base:
             coarse_window = Window(coarse_cfg)
             periods = [{"range": [step, step]} for step in coarse_window.steps]
-            windows.append({
-                "window_operation": "sum",
-                "periods": periods,
-            })
+            windows.append(
+                {
+                    "window_operation": "sum",
+                    "periods": periods,
+                }
+            )
 
         return {"windows": windows}
 
@@ -150,7 +177,9 @@ class ParamRequester:
         self.loc = loc
         self.members = members
 
-    def retrieve_data(self, fdb, step: AnyStep) -> Tuple[eccodes.GRIBMessage, np.ndarray]:
+    def retrieve_data(
+        self, fdb, step: AnyStep
+    ) -> Tuple[eccodes.GRIBMessage, np.ndarray]:
         in_keys = self.param.in_keys(step=step)
         return read_ensemble(self.sources, self.loc, self.members, **in_keys)
 
@@ -163,26 +192,41 @@ class QuantilesConfig(Config):
     def __init__(self, args: argparse.Namespace, verbose: bool = True):
         super().__init__(args, verbose=verbose)
 
-        self.num_members = self.options.get('num_members', 51)
-        self.num_quantiles = self.options.get('num_quantiles', 100)
+        self.num_members = self.options.get("num_members", 51)
+        self.num_quantiles = self.options.get("num_quantiles", 100)
 
-        self.out_keys = self.options.get('out_keys', {})
+        self.out_keys = self.options.get("out_keys", {})
 
-        self.params = [ParamConfig(pname, popt) for pname, popt in self.options['params'].items()]
-        self.windows = self.options.get('windows', [])
+        self.params = [
+            ParamConfig(pname, popt) for pname, popt in self.options["params"].items()
+        ]
+        self.windows = self.options.get("windows", [])
 
-        self.sources = self.options.get('sources', {})
+        self.sources = self.options.get("sources", {})
 
         self.n_par_read = self.options.get("n_par_read", 1)
         self.n_par_compute = self.options.get("n_par_compute", 1)
         self.window_queue_size = self.options.get("queue_size", self.n_par_compute)
 
 
-def quantiles_iteration(config: QuantilesConfig, param: ParamConfig, target: Target, template: Union[str, eccodes.GRIBMessage], window: Window):
+def quantiles_iteration(
+    config: QuantilesConfig,
+    param: ParamConfig,
+    target: Target,
+    template: Union[str, eccodes.GRIBMessage],
+    window: Window,
+):
     if not isinstance(template, eccodes.GRIBMessage):
         template = read_template(template)
     with ResourceMeter(f"{param.name}, step {window.name}: Quantiles"):
-        do_quantiles(window.step_values, template, target, param.out_paramid, n=config.num_quantiles, out_keys=window.grib_header())
+        do_quantiles(
+            window.step_values,
+            template,
+            target,
+            param.out_paramid,
+            n=config.num_quantiles,
+            out_keys=window.grib_header(),
+        )
 
 
 def main(args: List[str] = sys.argv[1:]):
@@ -202,9 +246,15 @@ def main(args: List[str] = sys.argv[1:]):
 
     with executor:
         for param in config.params:
-            requester = ParamRequester(param, config.sources, args.in_ens, config.num_members)
-            window_manager = WindowManager(param.window_config(config.windows), param.out_keys(config.out_keys))
-            quantiles_partial = functools.partial(quantiles_iteration, config, param, target)
+            requester = ParamRequester(
+                param, config.sources, args.in_ens, config.num_members
+            )
+            window_manager = WindowManager(
+                param.window_config(config.windows), param.out_keys(config.out_keys)
+            )
+            quantiles_partial = functools.partial(
+                quantiles_iteration, config, param, target
+            )
             for step, data in parallel_data_retrieval(
                 config.n_par_read,
                 window_manager.unique_steps,
@@ -220,5 +270,5 @@ def main(args: List[str] = sys.argv[1:]):
             executor.wait()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
