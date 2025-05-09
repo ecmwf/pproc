e73d8ff418f6d384f3d9d70325a25cf22305e90c
Jenny Wong <jenny.wong@ecmwf.int>
2025-05-09 07:31:15 +0000

    Revert thermofeel output metadata

--- src/pproc/config/io.py
+++ src/pproc/config/io.py
@@ -275,12 +275,5 @@ WindOutputModel = create_output_model(
     {"mean": {"type": "em"}, "std": {"type": "es"}, "ws": {}},
 )
 ThermoSourceModel = create_source_model("Thermo", ["inst"], optional=["accum"])
-ThermoOutputModel = create_output_model(
-    "Thermo",
-    {
-        "indices": {"edition": 2},
-        "accum": {},
-        "intermediate": {},
-    },
-)
+ThermoOutputModel = create_output_model("Thermo", ["indices", "accum", "intermediate"])
 ECPointOutputModel = create_output_model("ECPoint", ["bs", "wt", "perc"])
