c1b0a046c86058701a387255214704660a39347d
Jenny Wong <jenny.wong@ecmwf.int>
2025-05-29 07:30:00 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name="pproc",
-    version='1.4.1',
+    version='1.4.2',
     author='ECMWF',
     description="ECMWF Post-processing tools",
     packages=find_packages(where='src'),
