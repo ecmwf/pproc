0869f741af9ef380f4e707968be6615a7155d4d8
Jenny Wong <jenny.wong@ecmwf.int>
2025-07-16 16:45:02 +0000

    Fix quantiles for ecpoint

--- src/pproc/schema/config.py
+++ src/pproc/schema/config.py
@@ -24,14 +24,18 @@ class ConfigSchema(BaseSchema):
 
     def config(self, output_request: dict) -> dict:
         config = self.traverse(output_request)
-        if output_request["type"] in ["pb", "cd"]:
+        if "quantile" in output_request:
             numbers = np.zeros(len(output_request["quantile"]))
             totals = np.zeros(len(output_request["quantile"]))
             for index, quantile in enumerate(output_request["quantile"]):
                 number, total = map(int, quantile.split(":"))
                 numbers[index] = number
                 totals[index] = total
-            if np.all(totals == totals[0]) and np.all(np.diff(numbers) == 1):
+            if (
+                np.all(totals == totals[0])
+                and np.all(np.diff(numbers) == 1)
+                and len(numbers) == total + 1
+            ):
                 quantiles = int(totals[0])
             else:
                 quantiles = numbers / totals
