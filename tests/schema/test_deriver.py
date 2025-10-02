0122854380430ec4656c4c23a87a400d3f67bef5
Jenny Wong <jenny.wong@ecmwf.int>
2025-10-02 14:58:23 +0000

    Add tests for clim step deriver

--- tests/schema/test_deriver.py
+++ tests/schema/test_deriver.py
@@ -1,6 +1,102 @@
 import pytest
 
-from pproc.schema.deriver import ClimDateDeriver
+from pproc.schema.deriver import ClimStepDeriver, ClimDateDeriver
+
+
+@pytest.mark.parametrize(
+    "step_type, time, step, clim_steps, expected",
+    [
+        [
+            "instantaneous",
+            "0000",
+            [12, 24, 360],
+            list(range(0, 361, 12)),
+            [12, 24, 360],
+        ],
+        [
+            "instantaneous",
+            "0600",
+            [12, 24, 360],
+            list(range(0, 361, 12)),
+            [12, 24, 360],
+        ],
+        [
+            "instantaneous",
+            "1200",
+            [12, 24, 360],
+            list(range(0, 361, 12)),
+            [24, 36, 348],
+        ],
+        [
+            "instantaneous",
+            "1800",
+            [12, 24, 360],
+            list(range(0, 361, 12)),
+            [24, 36, 348],
+        ],
+        [
+            "range",
+            "0000",
+            list(range(0, 25, 12)),
+            ["0-24", "12-36", "24-28", "0-240"],
+            "0-24",
+        ],
+        [
+            "range",
+            "0000",
+            list(range(0, 241, 12)),
+            ["0-24", "12-36", "24-28", "0-240"],
+            "0-240",
+        ],
+        [
+            "range",
+            "0600",
+            list(range(0, 25, 12)),
+            ["0-24", "12-36", "24-28", "0-240"],
+            "0-24",
+        ],
+        [
+            "range",
+            "0600",
+            list(range(0, 241, 12)),
+            ["0-24", "12-36", "24-28", "0-240"],
+            "0-240",
+        ],
+        [
+            "range",
+            "1200",
+            list(range(0, 25, 12)),
+            ["0-24", "12-36", "24-28", "0-240"],
+            "12-36",
+        ],
+        [
+            "range",
+            "1200",
+            list(range(0, 241, 12)),
+            ["0-24", "12-36", "24-28", "0-240"],
+            "0-240",
+        ],
+        [
+            "range",
+            "1800",
+            list(range(0, 25, 12)),
+            ["0-24", "12-36", "24-28", "0-240"],
+            "12-36",
+        ],
+        [
+            "range",
+            "1800",
+            list(range(0, 241, 12)),
+            ["0-24", "12-36", "24-28", "0-240"],
+            "0-240",
+        ],
+        ["range", "0000", ["0-168"], ["0-168", "24-192", "48-216"], "0-168"],
+        ["range", "0000", ["24-192"], ["0-168", "24-192", "48-216"], "24-192"],
+    ],
+)
+def test_clim_step_deriver(step_type, time, step, clim_steps, expected):
+    deriver = ClimStepDeriver(type=step_type)
+    assert deriver.derive({"time": time, "step": step}, clim_steps) == expected
 
 
 @pytest.mark.parametrize(
