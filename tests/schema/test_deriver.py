f670a1d2e9cd254dea4036a4b007be5106452320
Jenny Wong <jenny.wong@ecmwf.int>
2025-10-02 14:27:43 +0000

    Fix tests

--- tests/schema/test_deriver.py
+++ tests/schema/test_deriver.py
@@ -6,9 +6,10 @@ from pproc.schema.deriver import ClimDateDeriver
 @pytest.mark.parametrize(
     "config, date, expected",
     [
-        [{"option": "next", "sequence": "daily"}, "20250930", ["20251001"]],
-        [{"option": "previous", "strict": False}, "20250930", ["20250929"]],
-        [{"option": "nearest"}, "20250930", ["20250929"]],
+        [{"option": "next", "sequence": "daily"}, "20250930", "20251001"],
+        [{"option": "previous", "strict": False}, "20250930", "20250929"],
+        [{"option": "previous"}, "20250929", "20250927"],
+        [{"option": "nearest"}, "20250930", "20250929"],
         [
             {"option": "bracket", "num": 1, "strict": False},
             "20250930",
@@ -18,4 +19,4 @@ from pproc.schema.deriver import ClimDateDeriver
 )
 def test_clim_date_deriver(config, date, expected):
     deriver = ClimDateDeriver(**config)
-    deriver.derive({"date": date}, "ecmwf-2days") == [expected]
+    assert deriver.derive({"date": date}, "ecmwf-2days") == expected
