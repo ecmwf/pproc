0f16847b4121f53577051b042ebe8c3077687fd6
Jenny Wong <jenny.wong@ecmwf.int>
2026-06-24 12:24:13 +0000

    Add metadata argument to difference_rate

--- pproc-runtime/src/ppruntime/accumulation.py
+++ pproc-runtime/src/ppruntime/accumulation.py
@@ -2,10 +2,11 @@ from earthkit.data import FieldList
 
 from ppruntime.utils import new_fieldlist
 
-def difference_rate(*fields, factor: float = 1.0) -> FieldList:
+
+def difference_rate(*fields, factor: float = 1.0, metadata: dict) -> FieldList:
     values = fields[-1].values
     length = fields[-1].metadata()["step"]
     if len(fields) != 1:
-        values = values - fields[0].values 
+        values = values - fields[0].values
         length = length - fields[0].metadata()["step"]
-    return new_fieldlist(values / (factor * length), [fields[-1].metadata()])
+    return new_fieldlist(values / (factor * length), [fields[-1].metadata()], metadata)
