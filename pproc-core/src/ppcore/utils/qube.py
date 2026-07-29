7c9abf765ba5056bf12373bc3dd69bcad937af65
Jenny Wong <jenny.wong@ecmwf.int>
2026-07-29 14:26:03 +0100

    Add licenses

--- pproc-core/src/ppcore/utils/qube.py
+++ pproc-core/src/ppcore/utils/qube.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2026 European Centre for Medium-Range Weather Forecasts (ECMWF)
+#
+# SPDX-License-Identifier: Apache-2.0
+
 from typing import Iterable
 
 from qubed import Qube
