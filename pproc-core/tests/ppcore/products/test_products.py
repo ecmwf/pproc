536da12c5cd967cd3495049388df95e7148c1650
Jenny Wong <jenny.wong@ecmwf.int>
2026-07-30 16:56:23 +0100

    Handle thermofeel from custom source

--- pproc-core/tests/ppcore/products/test_products.py
+++ pproc-core/tests/ppcore/products/test_products.py
@@ -12,13 +12,83 @@ from earthkit.workflows.fluent import merge
 
 from earthkit.workflows.plugins.pproc.fluent import from_source
 from earthkit.workflows.plugins.pproc.utils.request import Request
-from ppcore.schema.forecast import ForecastDefinition
+from ppcore.schema.forecast import definition_to_dataset, ForecastDefinition
 from ppcore.products import product_from_output, graph_from_outputs, action_from_outputs
 from ppcore.utils.requests import expand
 from conftest import SCHEMA
 
 
 ROOT_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)))
+
+
+INPUTS = {
+    "ensemble": [
+        {
+            "stream": "oper",
+            "type": "fc",
+            "step": list(range(0, 24, 3)),
+            "param": ["228", "167"],
+            "levtype": "sfc",
+        },
+        {
+            "stream": "enfo",
+            "type": "pf",
+            "step": list(range(0, 24, 3)),
+            "param": ["228", "167"],
+            "number": list(range(1, 5)),
+            "levtype": "sfc",
+        },
+        {
+            "stream": "oper",
+            "type": "fc",
+            "step": list(range(0, 24, 3)),
+            "param": ["130"],
+            "levtype": "pl",
+            "levelist": [250, 500, 850],
+        },
+        {
+            "stream": "enfo",
+            "type": "pf",
+            "step": list(range(0, 24, 3)),
+            "param": ["130"],
+            "levtype": "pl",
+            "levelist": [250, 500, 850],
+            "number": list(range(1, 5)),
+        },
+    ],
+    "deterministic": [
+        {
+            "stream": "oper",
+            "type": "fc",
+            "step": [0, 1, 2],
+            "param": [
+                "47",
+                "165",
+                "166",
+                "167",
+                "168",
+                "169",
+                "175",
+                "176",
+                "177",
+                "228021",
+            ],
+            "levtype": "sfc",
+            "date": "20260721",
+            "time": "1200",
+        },
+        {
+            "stream": "oper",
+            "type": "fc",
+            "step": list(range(0, 24, 3)),
+            "param": ["130"],
+            "levtype": "pl",
+            "levelist": [250, 500, 850],
+            "date": "20260721",
+            "time": "1200",
+        },
+    ],
+}
 
 
 @pytest.mark.parametrize(
@@ -44,78 +114,44 @@ def test_action_construction():
 
 
 @pytest.mark.parametrize(
-    "requests",
+    "input_name, requests",
     [
-        os.path.join(ROOT_DIR, "templates", "ensms.yaml"),
-        os.path.join(ROOT_DIR, "templates", "quantiles.yaml"),
+        ["ensemble", os.path.join(ROOT_DIR, "templates", "ensms.yaml")],
+        ["ensemble", os.path.join(ROOT_DIR, "templates", "quantiles.yaml")],
+        ["deterministic", os.path.join(ROOT_DIR, "templates", "thermo.yaml")],
     ],
-    ids=["ensms", "quantiles"],
+    ids=["ensms", "quantiles", "thermo"],
 )
-def test_custom_source(requests):
+def test_custom_source(input_name, requests):
     with open(requests, "r") as f:
         output_requests = yaml.safe_load(f)
 
     graph = Graph([])
     defaults = {"class": "od", "date": "20230914", "time": "1200", "expver": "0001"}
     for req in expand(output_requests):
-        inputs = [
-            {
-                **defaults,
-                "stream": "oper",
-                "type": "fc",
-                "step": list(range(0, 24, 3)),
-                "param": ["228", "167"],
-                "levtype": "sfc",
-            },
-            {
-                **defaults,
-                "stream": "enfo",
-                "type": "pf",
-                "step": list(range(0, 24, 3)),
-                "param": ["228", "167"],
-                "number": list(range(1, 5)),
-                "levtype": "sfc",
-            },
-            {
-                **defaults,
-                "stream": "oper",
-                "type": "fc",
-                "step": list(range(0, 24, 3)),
-                "param": ["130"],
-                "levtype": "pl",
-                "levelist": [250, 500, 850],
-            },
-            {
-                **defaults,
-                "stream": "enfo",
-                "type": "pf",
-                "step": list(range(0, 24, 3)),
-                "param": ["130"],
-                "levtype": "pl",
-                "levelist": [250, 500, 850],
-                "number": list(range(1, 5)),
-            },
-        ]
+        inputs = [{**defaults, **inp} for inp in INPUTS[input_name]]
+        forecast = ForecastDefinition(
+            datacubes=inputs, unperturbed={"stream": "oper", "type": "fc"}
+        )
         product = product_from_output(
             req,
             SCHEMA,
-            forecast=ForecastDefinition(
-                datacubes=inputs, unperturbed={"stream": "oper", "type": "fc"}
-            ),
+            forecast=forecast,
             metadata={"edition": 2},
         )
         source_actions = []
+        is_ensemble = definition_to_dataset(forecast).is_ensemble
         for levtype in ["sfc", "pl"]:
             for request in inputs:
                 if request["levtype"] != levtype:
                     continue
                 req = Request(request)
-                if "number" not in req:
+                if is_ensemble and "number" not in req:
                     req.make_dim("number", 0)
                 new_action = from_source(
                     ["fdb"],
                     [req],
-                    join_key="number",
+                    join_key="number" if is_ensemble else "",
                 )
                 new_action = new_action.set_path(f"/levtype={levtype}/{req['param']}")
                 source_actions.append(new_action)
